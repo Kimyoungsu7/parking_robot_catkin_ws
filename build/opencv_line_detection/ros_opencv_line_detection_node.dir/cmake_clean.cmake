8afae95c1638468010bb07edf5c198598aef8099
Kimyoungsu7 <159991311+Kimyoungsu7@users.noreply.github.com>
2025-02-07 20:54:13 +0900

    Add files via upload

---
+++ build/opencv_line_detection/ros_opencv_line_detection_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_opencv_line_detection_node.dir/src/opencv_line_detect.cpp.o"
+  "/home/amap/aMAP_catkin_ws/devel/lib/opencv_line_detection/ros_opencv_line_detection_node.pdb"
+  "/home/amap/aMAP_catkin_ws/devel/lib/opencv_line_detection/ros_opencv_line_detection_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_opencv_line_detection_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
